a148bb3315889095ddbb3973352140fda1954ec1
Boby Darmawan <90325160+bobydarmawan295@users.noreply.github.com>
2022-04-03 21:39:20 +0700

    update oddEven.c

--- src/C/oddEven.c
+++ src/C/oddEven.c
@@ -5,16 +5,15 @@ int main()
 {
     int i = 0;
     printf("Bilangan Ganjil Menggunakan Perulangan FOR\n");
-      for(int i = 1; i <= 100; i = i+2){
-       if(i % 1 == 0)
+      for(int i = 1; i <= 100; i++){
+        if(i % 2 == 1)
          printf("%d\n", i);
 
     }
     printf("Bilangan Genap Menggunakan Perulangan FOR\n");
-    for(int i = 1; i <= 100; i= i+1){
-      if(i % 2 == 0)
-         printf("%d\n", i);
-
+    for(int i = 1; i <= 100; i++){
+        if(i % 2 == 0)
+            printf("%d\n", i);
     }
     return 0;
 }
